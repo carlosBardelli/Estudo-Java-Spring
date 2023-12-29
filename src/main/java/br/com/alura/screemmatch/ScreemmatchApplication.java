4a48f0de97c33e76ee33e9a1aa8415e1e735d3bb
carlosBardelli <carloshbardelli@yahoo.com.br>
2023-12-29 14:27:17 -0300

    Implementacao dos Dados dos Epsodios

--- src/main/java/br/com/alura/screemmatch/ScreemmatchApplication.java
+++ src/main/java/br/com/alura/screemmatch/ScreemmatchApplication.java
@@ -1,6 +1,9 @@
 package br.com.alura.screemmatch;
 
+import br.com.alura.screemmatch.model.DadosEpisodio;
+import br.com.alura.screemmatch.model.DadosSerie;
 import br.com.alura.screemmatch.service.ConsumoApi;
+import br.com.alura.screemmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +18,13 @@ public class ScreemmatchApplication  implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		ConsumoApi consumoApi = new ConsumoApi();
-		String json = consumoApi.obterDados("https://viacep.com.br/ws/01001000/json/");
+		String json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
 		System.out.println(json);
+		ConverteDados converteDados = new ConverteDados();
+		DadosSerie dadosSerie = converteDados.obterDados(json,DadosSerie.class);
+		System.out.println(dadosSerie);
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+		DadosEpisodio dadosEpisodio = converteDados.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 	}
 }
